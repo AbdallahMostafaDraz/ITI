f3e30bbe1f960238775cfd1cf93840e92426e586
Abdallah Mostafa <abdallah.mostafa.draz@gmail.com>
2025-02-17 21:16:45 +0200

    Add MVC Course

---
+++ ASP.NET Core MVC/Lab1/Lab1/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 4 
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+
+        public string ShowStudent(Student student)
+        {
+            return student.ToString();
+        }
+    }
+}
---
+++ ASP.NET Core MVC/Lab1/Lab1/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class TestController : Controller
+    {
+        // 1
+        public string Display()
+        {
+            return "Welcome To MVC";
+        }
+
+        // 2
+        public ViewResult Show()
+        {
+            return View();
+        }
+
+
+        // 3
+        public ViewResult ShowAdd()
+        {
+            return View();
+        }
+
+        public int ShowResult(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
